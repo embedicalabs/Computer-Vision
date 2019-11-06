44f78c2c4fd8ac5a2ae53eadac1769191db539e1
xcloudhub <xcloudhub.biz@gmail.com>
2019-11-06 14:45:30 +0300

    Initialize

---
+++ ML-Tutorial/main.py
@@ -0,0 +1,8 @@
+from tensorflow import keras
+import numpy as np
+
+model=keras.Sequential([keras.layers.Dense(units=1,input_shape=[1])])
+model.compile(optimizer="sgd",loss="mse")
+
+xs=np.array([-1.0,0.0,1.0,2.0,3.0,4.0],dtype=float)
+ys=np.array([-3.0,-1.0,1.1,3.0,5.0,7.0],dtype=float)
